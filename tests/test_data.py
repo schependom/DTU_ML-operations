3f64e711e14e4c422cd1f9cea824d0ca2e60fc15
Vincent Van Schependom <vincent.van.schependom@gmail.com>
2026-01-06 16:26:36 +0100

    Added mock data for unit tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -5,5 +5,5 @@ from ml_ops.data import MyDataset
 
 def test_my_dataset():
     """Test the MyDataset class."""
-    dataset = MyDataset("data/raw")
+    dataset = MyDataset("data-mock/raw")
     assert isinstance(dataset, Dataset)
