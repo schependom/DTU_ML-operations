4aef632a9122a14879780444205ec023d27d71ef
Vincent Van Schependom <vincent.van.schependom@gmail.com>
2026-01-12 10:04:49 +0100

    Updated DVC SSH workflow

---
+++ src/ml_ops/dataset.py
@@ -0,0 +1,107 @@
+from __future__ import annotations
+
+import os
+from typing import TYPE_CHECKING
+
+import matplotlib.pyplot as plt
+import torch
+import typer
+from torch import Tensor
+from torch.utils.data import Dataset
+
+from ml_ops.utils import show_image_and_target
+
+if TYPE_CHECKING:
+    import torchvision.transforms.v2 as transforms
+
+
+class MnistDataset(Dataset):
+    """MNIST dataset for PyTorch.
+
+    Args:
+        data_folder: Path to the data folder.
+        train: Whether to load training or test data.
+        img_transform: Image transformation to apply.
+        target_transform: Target transformation to apply.
+    """
+
+    name: str = "MNIST"
+
+    def __init__(
+        self,
+        data_folder: str = "data",
+        train: bool = True,
+        img_transform: transforms.Transform | None = None,
+        target_transform: transforms.Transform | None = None,
+    ) -> None:
+        super().__init__()
+        self.data_folder = data_folder
+        self.train = train
+        self.img_transform = img_transform
+        self.target_transform = target_transform
+        self.load_data()
+
+    def load_data(self) -> None:
+        """Load images and targets from disk."""
+        images, target = [], []
+        if self.train:
+            nb_files = len([f for f in os.listdir(self.data_folder) if f.startswith("train_images")])
+            for i in range(nb_files):
+                images.append(torch.load(f"{self.data_folder}/train_images_{i}.pt"))
+                target.append(torch.load(f"{self.data_folder}/train_target_{i}.pt"))
+        else:
+            images.append(torch.load(f"{self.data_folder}/test_images.pt"))
+            target.append(torch.load(f"{self.data_folder}/test_target.pt"))
+        self.images = torch.cat(images, 0)
+        self.target = torch.cat(target, 0)
+
+    def __getitem__(self, index) -> tuple[Tensor, Tensor]:
+        """Return image and target tensor."""
+        img, target = self.images[index], self.target[index]
+        if self.img_transform:
+            img = self.img_transform(img)
+        if self.target_transform:
+            target = self.target_transform(target)
+        return img, target
+
+    def __len__(self) -> int:
+        """Return the number of images in the dataset."""
+        return self.images.shape[0]
+
+
+def dataset_statistics(datadir: str = "data") -> None:
+    """Compute dataset statistics."""
+    train_dataset = MnistDataset(data_folder=datadir, train=True)
+    test_dataset = MnistDataset(data_folder=datadir, train=False)
+    print(f"Train dataset: {train_dataset.name}")
+    print(f"Number of images: {len(train_dataset)}")
+    print(f"Image shape: {train_dataset[0][0].shape}")
+    print("\n")
+    print(f"Test dataset: {test_dataset.name}")
+    print(f"Number of images: {len(test_dataset)}")
+    print(f"Image shape: {test_dataset[0][0].shape}")
+
+    show_image_and_target(train_dataset.images[:25], train_dataset.target[:25], show=False)
+    plt.savefig("mnist_images.png")
+    plt.close()
+
+    train_label_distribution = torch.bincount(train_dataset.target)
+    test_label_distribution = torch.bincount(test_dataset.target)
+
+    plt.bar(torch.arange(10), train_label_distribution)
+    plt.title("Train label distribution")
+    plt.xlabel("Label")
+    plt.ylabel("Count")
+    plt.savefig("train_label_distribution.png")
+    plt.close()
+
+    plt.bar(torch.arange(10), test_label_distribution)
+    plt.title("Test label distribution")
+    plt.xlabel("Label")
+    plt.ylabel("Count")
+    plt.savefig("test_label_distribution.png")
+    plt.close()
+
+
+if __name__ == "__main__":
+    typer.run(dataset_statistics)
